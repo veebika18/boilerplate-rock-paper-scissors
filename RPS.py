997286dd398a8adec53f88c7c6083be5679574f4
veebika18 <veebika1803@gmail.com>
2025-01-19 18:32:31 +0530

    Update RPS

--- RPS.py
+++ RPS.py
@@ -1,10 +1,23 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
-
 def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
-
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
-
-    return guess
+    if prev_play:
+        opponent_history.append(prev_play)
+    
+    # Default prediction and counter
+    if len(opponent_history) < 3:
+        return "R"
+    
+    # Get the last three moves as a pattern
+    last_three = "".join(opponent_history[-3:])
+    
+    # Predict the next move based on patterns
+    patterns = {"R": 0, "P": 0, "S": 0}
+    for i in range(len(opponent_history) - 3):
+        if "".join(opponent_history[i:i + 3]) == last_three:
+            next_move = opponent_history[i + 3]
+            patterns[next_move] += 1
+    
+    # Choose the most common predicted move or default to "R"
+    predicted_move = max(patterns, key=patterns.get) if patterns else "R"
+    
+    # Return the counter move
+    return {"R": "P", "P": "S", "S": "R"}[predicted_move]
